145cecfee6f65085662de2a4acdbc28a3b601d41
David Mc Ken <david.mcken@gmail.com>
2024-11-03 11:47:32 -0400

    Print multiple platforms.

--- print_info/print_info.cpp
+++ print_info/print_info.cpp
@@ -5,14 +5,30 @@
  * g++ -O3 -o print_info.exe print_info.cpp -I/opt/rocm/include/ -L /opt/rocm/lib/ -lOpenCL
  * ./print_info.exe
  */
-
+// #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #define CL_TARGET_OPENCL_VERSION 220
-
 
 #include <CL/cl.hpp>
 #include <iostream>
+#include <string>
 
-int main(){
+
+std::string parse_devtype(unsigned int devtype)
+{
+    std::string devtypestr;
+    switch(devtype) {
+        case CL_DEVICE_TYPE_DEFAULT:     devtypestr = "default"; break;
+        case CL_DEVICE_TYPE_CPU:         devtypestr = "CPU";     break;
+        case CL_DEVICE_TYPE_GPU:         devtypestr = "GPU";     break;
+        case CL_DEVICE_TYPE_ACCELERATOR: devtypestr = "accel";   break;
+        case CL_DEVICE_TYPE_CUSTOM:      devtypestr = "custom";  break;
+        case CL_DEVICE_TYPE_ALL:         devtypestr = "all";     break;
+    }
+    return devtypestr;
+}
+
+int main()
+{
 
     /**
      * Search for all the OpenCL platforms available and check
@@ -28,42 +44,44 @@ int main(){
     }
 
     /**
-     * Search for all the devices on the first platform and check if
-     * there are any available.
-     * */
-
-    auto platform = platforms.front();
-    std::vector<cl::Device> devices;
-    platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
-
-    if (devices.empty()){
-        std::cerr << "No devices found!" << std::endl;
-        return -1;
-    }
-
-    /**
-     * Select the first device and print its information.
+     * Iterate over platforms.
      */
-    auto device       = devices.front();
-    auto name         = device.getInfo<CL_DEVICE_NAME>();
-    auto vendor       = device.getInfo<CL_DEVICE_VENDOR>();
-    auto version      = device.getInfo<CL_DEVICE_VERSION>();
-    auto workItems    = device.getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>();
-    auto workGroups   = device.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>();
-    auto computeUnits = device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>();
-    auto globalMemory = device.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>();
-    auto localMemory  = device.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>();
+    for( auto platform = platforms.begin(); platform != platforms.end(); ++platform )
+    {
+        std::vector<cl::Device> devices;
+        platform->getDevices(CL_DEVICE_TYPE_ALL, &devices);
 
-    std::cout << "OpenCL Device Info:"
-    << "\nName: " << name
-    << "\nVendor: " << vendor
-    << "\nVersion: " << version
-    << "\nMax size of work-items: (" << workItems[0] << "," << workItems[1] << "," << workItems[2] << ")"
-    << "\nMax size of work-groups: " << workGroups
-    << "\nNumber of compute units: " << computeUnits
-    << "\nGlobal memory size (bytes): " << globalMemory
-    << "\nLocal memory size per compute unit (bytes): " << localMemory/computeUnits
-    << std::endl;
+        if (devices.empty()){
+            std::cerr << "No devices found!" << std::endl;
+            return -1;
+        }
+
+        /**
+         * Print current device information.
+         */
+        auto device       = devices.front();
+        auto name         = device.getInfo<CL_DEVICE_NAME>();
+        auto devtype      = device.getInfo<CL_DEVICE_TYPE>();
+        auto vendor       = device.getInfo<CL_DEVICE_VENDOR>();
+        auto version      = device.getInfo<CL_DEVICE_VERSION>();
+        auto workItems    = device.getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>();
+        auto workGroups   = device.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>();
+        auto computeUnits = device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>();
+        auto globalMemory = device.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>();
+        auto localMemory  = device.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>();
+
+        std::cout << "OpenCL Device Info: "
+            << "\nName:      " << name
+            << "\nType:      " << parse_devtype(devtype)
+            << "\nVendor:    " << vendor
+            << "\nVersion:   " << version
+            << "\nMax size of work-items:    (" << workItems[0] << "," << workItems[1] << "," << workItems[2] << ")"
+            << "\nMax size of work-groups:    " << workGroups
+            << "\nNumber of compute units:    " << computeUnits
+            << "\nGlobal memory size (bytes): " << globalMemory
+            << "\nLocal memory size per compute unit (bytes): " << localMemory/computeUnits
+            << "\n" << std::endl;
+    }
 
     return 0;
 }
